6f30bcfe460b0948a191e71c88719dcd3f731c47
Efe Arda Sakarya <efeardasakarya@gmail.com>
2025-08-27 10:47:15 +0300

    Latch added

--- ConcurrentTaskProcessor/CorruptedThread.cpp
+++ ConcurrentTaskProcessor/CorruptedThread.cpp
@@ -1,6 +1,7 @@
 ﻿#include "CorruptedThread.h"
 #include <chrono>
 #include <iostream>
+#include <latch>
 
 
 CorruptedThread::CorruptedThread(int threads)
@@ -10,21 +11,27 @@ CorruptedThread::CorruptedThread(int threads)
 
 void CorruptedThread::createWorkersByAddFunction()
 {
-	//Corrupted
-	; // start before thread creating
+	std::latch done(threads);
+
+
+
 	auto start = std::chrono::high_resolution_clock::now();
 	for (int i = 0; i < threads; i++)
 	{
 
-		workers.emplace_back([&] { add(); });
+		workers.emplace_back([&] { add(); done.count_down(); });
 		
 	}
+
+	done.wait();
+
+	workers.clear();
 
 	auto end = std::chrono::high_resolution_clock::now();
 
 	auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
 
-	std::cout << "Workers created " << double(duration.count()) / 1000.0 << " milisecond" << '\n';
+	std::cout << "Workers created and destroyed " << double(duration.count()) / 1000.0 << " milisecond" << '\n';
 
 	std::cout << victim << '\n';
 	
